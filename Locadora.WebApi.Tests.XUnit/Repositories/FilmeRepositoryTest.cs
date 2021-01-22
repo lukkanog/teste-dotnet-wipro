56cd35e2a6e9bdfde6821ffb3f8e2d7fc1e675c8
lukkanog <lukanog@hotmail.com>
2021-01-22 16:02:06 -0300

    testes repositorio de filmes

---
+++ Locadora.WebApi.Tests.XUnit/Repositories/FilmeRepositoryTest.cs
@@ -0,0 +1,40 @@
+﻿using Locadora.WebApi.Interfaces;
+using Locadora.WebApi.Models;
+using Locadora.WebApi.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Locadora.WebApi.Tests.XUnit.Repositories
+{
+    public class FilmeRepositoryTest
+    {
+        private IFilmeRepository filmeRepository { get; set; }
+
+        public FilmeRepositoryTest()
+        {
+            filmeRepository = new FilmeRepository();
+        }
+
+        [Fact]
+        public void TestaListagemFilmes()
+        {
+            List<Filme> filmes = filmeRepository.ListarFilmes();
+            Assert.NotNull(filmes);
+        }
+
+        [Fact]
+        public void TestaCadastroFilme()
+        {
+            Filme filme = new Filme();
+            filme.Nome = "Filme Teste";
+
+            Filme filmeCadastrado = filmeRepository.CadastrarFilme(filme);
+            Assert.NotNull(filmeCadastrado);
+            Assert.Equal(filme, filmeCadastrado);
+        }
+
+
+    }
+}
